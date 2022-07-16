d62bd0fcf1555c74134511444d99f57f6a7bdf23
Huynh Thai Phong <bon2762001@gmail.com>
2022-07-16 23:40:08 +0700

    user page ui commit
    haha

--- ParkingManagementNew/app/src/main/java/com/example/parking/UserPage.java
+++ ParkingManagementNew/app/src/main/java/com/example/parking/UserPage.java
@@ -1,14 +1,41 @@
 package com.example.parking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.parking.databinding.ActivityUserPageBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UserPage extends AppCompatActivity {
+
+    private ActivityUserPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_page);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        binding = ActivityUserPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_notifications, R.id.navigation_aboutUs, R.id.navigation_user_page)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 }
